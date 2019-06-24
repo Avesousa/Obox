2e981f714b03c29fb20f5d124bd95d6c70112d02
unknown <avesousa14@gmail.com>
2019-06-24 07:43:29 -0300

    version del 24/06 en la mañana

--- script/animacion.js
+++ script/animacion.js
@@ -5,49 +5,103 @@ $(document).ready(function(){
         var rot = $(".avRotar").offset().top;
         var der = $(".avDer").offset().top;
         var izq = $(".avIzq").offset().top;
+        var mostD = $(".avMostrarD").offset().top;
+       // var mostE = $(".avMostrarE").offset().top;
+        var rotD = $(".avRotarD").offset().top;
+       // var rotE = $(".avRotarE").offset().top;
+        var derD = $(".avDerD").offset().top;
+       // var derE = $(".avDerE").offset().top;
+        var izqD = $(".avIzqD").offset().top;
+       // var izqE = $(".avIzqE").offset().top;
+        var mayor = 600;
+        var menor = 400;
         //var top = $(".avTop").offset().top;
         //var bot = $(".avBottom").offset().top;
-        console.log("ANIMADOR");
         console.log(ubi);
-        if((most-400) <= ubi && (most+400) >= ubi){
+        if((most-menor) <= ubi && (most+mayor) >= ubi){
             $(".avMostrar").addClass("aparecer");
+            $(".avMostrar").addClass("avMostrarT");
         }
         else{
             $(".avMostrar").removeClass("aparecer");
+            $(".avMostrar").removeClass("avMostrarT");
         }
-        if((rot-400) <= ubi && (rot+400) >= ubi){
+        if((rot-menor) <= ubi && (rot+mayor) >= ubi){
             $(".avRotar").addClass("rotar");
+            $(".avRotar").addClass("avRotarT");
         }else{
             $(".avRotar").removeClass("rotar");
+            $(".avRotar").removeClass("avRotarT");
         }
-        if((der-400) <= ubi && (der+400) >= ubi){
+        if((der-menor) <= ubi && (der+mayor) >= ubi){
             $(".avDer").addClass("haciaDerecha");
+            $(".avDer").addClass("avDerT");
         }else{
             $(".avDer").removeClass("haciaDerecha");
+            $(".avDer").removeClass("avDerT");
         }
-        if((izq-400) <= ubi && (izq+400) >= ubi){
+        if((izq-menor) <= ubi && (izq+mayor) >= ubi){
             $(".avIzq").addClass("haciaIzquierda");
+            $(".avIzq").addClass("avIzqT");
         }else{
             $(".avIzq").removeClass("haciaIzquierda");
+            $(".avIzq").removeClass("avIzqT");
         }
-        //if((top-300) <= ubi && (top+300) >= ubi){
-        //    console.log("agrego");
-        //    $(".avTop").addClass("haciaArriba");
-        ///}else{
-        //    console.log("quito");
-        //    $(".avTop").removeClass("haciaArriba");
+        if((mostD-menor) <= ubi && (mostD+mayor) >= ubi){
+            $(".avMostrarD").addClass("aparecer");
+            $(".avMostrarD").addClass("avMostrarT");
+        }else{
+            $(".avMostrarD").removeClass("aparecer");
+            $(".avMostrarD").removeClass("avMostrarT");
+        }
+        ///if((mostE-menor) <= ubi && (mostE+mayor) >= ubi){
+        //    $(".avMostrarE").addClass("aparecer");
+        //    $(".avMostrarE").addClass("avMostrarT");
+        //}else{
+        //    $(".avMostrarE").removeClass("aparecer");
+        //    $(".avMostrarE").removeClass("avMostrarT");
         //}
-        //if(ubi >= bot)
-         //   $(".avBottom").addClass("haciaAbajo");
+        if((rotD-menor) <= ubi && (rotD+mayor) >= ubi){
+            $(".avRotarD").addClass("rotar");
+            $(".avRotarD").addClass("avRotarT");
+        }else{
+            $(".avRotarD").removeClass("rotar");
+            $(".avRotarD").removeClass("avRotarT");
+        }
+        //if((rotE-menor) <= ubi && (rotE+mayor) >= ubi){
+        //    $(".avRotarE").addClass("rotar");
+        //    $(".avRotarE").addClass("avRotarT");
+        //}else{
+        //    $(".avRotarE").removeClass("rotar");
+        //    $(".avRotarE").removeClass("avRotarT");
+        //}
+        if((derD-menor) <= ubi && (derD+mayor) >= ubi){
+            $(".avDerD").addClass("haciaDerecha");
+            $(".avDerD").addClass("avDerT");
+        }else{
+            $(".avDerD").removeClass("haciaDerecha");
+            $(".avDerD").removeClass("avDerT");
+        }
+        //if((derE-menor) <= ubi && (derE+mayor) >= ubi){
+        //    $(".avDerE").addClass("haciaDerecha");
+        //    $(".avDerE").addClass("avDerT");
+        //}else{
+        //    $(".avDerE").removeClass("haciaDerecha");
+        //    $(".avDerE").removeClass("avDerT");
+        //}
+        if((izqD-menor) <= ubi && (izqD+mayor) >= ubi){
+            $(".avIzqD").addClass("haciaIzquierda");
+            $(".avIzqD").addClass("avIzqT");
+        }else{
+            $(".avIzqD").removeClass("haciaIzquierda");
+            $(".avIzqD").removeClass("avIzqT");
+        }
+        //if((izqE-menor) <= ubi && (izqE+mayor) >= ubi){
+        //    $(".avIzqE").addClass("haciaIzquierda");
+        //    $(".avIzqE").addClass("avIzqT");
+        //}else{
+        //    $(".avIzqE").removeClass("haciaIzquierda");
+        //    $(".avIzqE").removeClass("avIzqT");
+        //}
     });
 })
-
-/*function animar(){
-    console.log("entro en animar");
-    $(".avMostrar").addClass("aparecer");
-    $(".avRotar").addClass("rotar");
-    $(".avDer").addClass("haciaDerecha");
-    $(".avIzq").addClass("haciaIzquierda");
-    $(".avTop").addClass("haciaArriba");
-    $(".avBottom").addClass("haciaAbajo");
-}*/
--- script/script.js
+++ script/script.js
@@ -1,10 +1,34 @@
 var bandera = true;
 var bajada = false;
 $(document).ready(function(){
+    $(".btn-movil").click(function(){
+        $("nav").css("display","inline-block");
+        $(".btn-menu").css("display","none");
+        $("nav").addClass("haciaDer");
+        $("nav").addClass("avDerT");
+    });
+
+    $(".cerrar").click(function(){
+        $(".btn-menu").css("display","inline-block");
+        $("nav").removeClass("haciaDer");
+        $("nav").removeClass("avDerT");
+        $("nav").addClass("haciaDerSalir");
+        //$("nav").css("transform","translateX(-20px)");
+        setTimeout(function(){
+            $("nav").css("display","none");
+            $("nav").removeClass("haciaDerSalir");
+        },2000);
+    });
+
     $(window).scroll(function(e){
         var num = $(window).scrollTop();
-        console.log(num);
-        if(num > 150 && bandera){
+        var vari;
+        if($(window).width() >= 768){
+            vari = 150;
+        } else{
+            vari = 80;
+        }
+        if(num > vari && bandera){
             //animar();
             $("#publicidad").css("display","inline-block");
             $("nav").css("box-shadow","0px 5px 5px 0px rgba(0,0,0,0.34)");
